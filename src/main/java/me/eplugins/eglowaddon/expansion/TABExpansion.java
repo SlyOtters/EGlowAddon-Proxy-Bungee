f0ac74cb39e53f680b77345f10fee61c59d93fba
MrGraycat <moorsn@gmail.com>
2025-02-23 07:14:46 +0100

    MiniMessages formatting is now automatically done by TAB

--- src/main/java/me/eplugins/eglowaddon/expansion/TABExpansion.java
+++ src/main/java/me/eplugins/eglowaddon/expansion/TABExpansion.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 import me.eplugins.eglowaddon.EGlowAddon;
 import me.neznamy.tab.api.TabAPI;
 import me.neznamy.tab.api.TabPlayer;
-import me.neznamy.tab.shared.chat.EnumChatFormat;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 
@@ -30,34 +29,26 @@ public class TABExpansion {
 		if (TabAPI.getInstance().getNameTagManager() != null) {
 			String tagPrefix = TabAPI.getInstance().getNameTagManager().getOriginalPrefix(tabPlayer);
 
-			if (tagPrefix.contains("<")) {
-				for (EnumChatFormat colorName : EnumChatFormat.values()) {
-					if (tagPrefix.toLowerCase().contains("<" + colorName.toString().toLowerCase())) {
-						glowColor = "<" + glowColor + ">";
-						break;
-					}
-				}
-			} else {
-				switch (glowColor.toLowerCase()) {
-					case "red" -> glowColor = color("&c");
-					case "dark_red" -> glowColor = color("&4");
-					case "gold" -> glowColor = color("&6");
-					case "yellow" -> glowColor = color("&e");
-					case "aqua" -> glowColor = color("&b");
-					case "dark_aqua" -> glowColor = color("&3");
-					case "blue" -> glowColor = color("&9");
-					case "dark_blue" -> glowColor = color("&1");
-					case "green" -> glowColor = color("&a");
-					case "dark_green" -> glowColor = color("&2");
-					case "light_purple" -> glowColor = color("&d");
-					case "dark_purple" -> glowColor = color("&5");
-					case "white" -> glowColor = color("&f");
-					case "gray" -> glowColor = color("&7");
-					case "dark_gray" -> glowColor = color("&8");
-					case "black" -> glowColor = color("&0");
-					case "reset" -> glowColor = "";
-				}
+			switch (glowColor.toLowerCase()) {
+				case "red" -> glowColor = color("&c");
+				case "dark_red" -> glowColor = color("&4");
+				case "gold" -> glowColor = color("&6");
+				case "yellow" -> glowColor = color("&e");
+				case "aqua" -> glowColor = color("&b");
+				case "dark_aqua" -> glowColor = color("&3");
+				case "blue" -> glowColor = color("&9");
+				case "dark_blue" -> glowColor = color("&1");
+				case "green" -> glowColor = color("&a");
+				case "dark_green" -> glowColor = color("&2");
+				case "light_purple" -> glowColor = color("&d");
+				case "dark_purple" -> glowColor = color("&5");
+				case "white" -> glowColor = color("&f");
+				case "gray" -> glowColor = color("&7");
+				case "dark_gray" -> glowColor = color("&8");
+				case "black" -> glowColor = color("&0");
+				case "reset" -> glowColor = "";
 			}
+
 			if (glowColor.isEmpty()) {
 				TabAPI.getInstance().getNameTagManager().setPrefix(tabPlayer, null);
 			} else {
